5ae9854cb70e2007bf0af9605f1563962092a31f
sungbin@nate.com <sungbin@nate.com>
2020-07-13 14:18:02 +0900

    머스태치 샘플 예제

---
+++ src/main/java/com/cos/demo/ViewresolverApplication.java
@@ -0,0 +1,14 @@
+package com.cos.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewresolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViewresolverApplication.class, args);
+	}
+
+}
+
